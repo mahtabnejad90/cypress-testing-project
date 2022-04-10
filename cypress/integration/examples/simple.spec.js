573e86aca269a9882c640eaa7bca7a073525af15
Mahtab Nejad <mahtab@bahmannejad.com>
2022-04-10 02:11:28 +0100

    combine selectors for new scenario

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,6 +1,4 @@
-/* const { describe } = require("mocha")
-
-describe('My first cypress test', () => {
+describe('Dummy test part 1', () => {
     it('True should be true', () => {
         expect(true).to.equal(true)
     })
@@ -10,16 +8,16 @@ describe('My first cypress test', () => {
     })
 })
 
-describe('Another dummy test', () => {
+describe('Dummy test part 2', () => {
     it('False should be false', () => {
         expect(false).to.equal(false)
     })
     it('10 should be 10', () => {
         expect(10).to.equal(10)
     })
-}) */
+})
 
-describe('browser actions', () => {
+ describe('Browser actions part 1', () => {
     it('should render correct URL', () => {
         cy.visit('http://example.com', {timeout: 10000})
     })
@@ -33,13 +31,27 @@ describe('browser actions', () => {
         cy.wait(3000)
     })
 
-    it('should pause the test execution', () => {
+/*     it('should pause the test execution', () => {
         //pauses test execution
         cy.pause()
-    })
+    }) */
 
     it('should check for correct element on the page ', () => {
         cy.get('h1').should('be.visible')
-    })
+    }) 
  })
 
+ describe('Browser actions part 2', () => {
+
+    it('should render correct book website URL', () => {
+        cy.visit('https://books.toscrape.com/index.html', {timeout: 10000})
+        cy.url().should('include', 'index.html')
+    })
+
+    it('should select the Travel category via click', () => {
+        cy.get('a')
+            .contains('Travel')
+            .click()
+        cy.get('h1').contains('Travel')
+    })
+})
