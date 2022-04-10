06d7db74005557d66db0c9f1ad8ff4c934983d1e
Mahtab Nejad <mahtab@bahmannejad.com>
2022-04-10 01:21:44 +0100

    add another dummy test

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,6 +1,18 @@
 describe('My first cypress test', () => {
     it('True should be true', () => {
-        expect(true).to.equal(false)
+        expect(true).to.equal(true)
+    })
+
+    it('5 should be 5', () => {
+        expect(5).to.equal(5)
     })
 })
 
+describe('Another dummy test', () => {
+    it('False should be false', () => {
+        expect(false).to.equal(false)
+    })
+    it('10 should be 10', () => {
+        expect(10).to.equal(10)
+    })
+})
