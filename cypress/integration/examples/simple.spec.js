f7cd79746b25acb1835ebfff159281c1164ddcda
Mahtab Nejad <mahtab@bahmannejad.com>
2022-04-10 01:51:21 +0100

    cypress implicit wait and pause commands

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -22,19 +22,24 @@ describe('Another dummy test', () => {
 describe('browser actions', () => {
     it('should render correct URL', () => {
         cy.visit('http://example.com', {timeout: 10000})
-
     })
 
     it('should check correct URL', () => {
-       
         cy.url().should('include', 'example.com')
+    })
 
+    it('should wait for 3 seconds', () => {
+        //implicit waiting
+        cy.wait(3000)
+    })
+
+    it('should pause the test execution', () => {
+        //pauses test execution
+        cy.pause()
     })
 
     it('should check for correct element on the page ', () => {
-       
         cy.get('h1').should('be.visible')
-
     })
  })
 
